bdea53f417020bd5fc8ba3c6d9c3f142466f7dcd
camarind <camarind@uniremingtonmanizales.edu.co>
2020-05-02 19:09:58 -0500

    intento 3

--- geoservi/settings.py
+++ geoservi/settings.py
@@ -115,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Bogota'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
